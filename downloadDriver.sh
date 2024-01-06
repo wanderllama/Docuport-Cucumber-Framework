4acf1e2cc550b40930ee682a3540febb5224790f
James Wallace <74025418+wanderllama@users.noreply.github.com>
2024-01-06 00:20:52 -0500

    found way to get latest msedgedriver version number to use in url so the latest msedgedriver is always downloaded will try to repeat for chrome and firefox

--- downloadDriver.sh
+++ downloadDriver.sh
@@ -17,75 +17,89 @@
 # https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-macos-aarch64.tar.gz
 # https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-linux64.tar.gz
 
+download() {
+  case $1 in
+  edge)
+    ls=LATEST_STABLE
+    curl -L -k --output "$ls" "https://msedgewebdriverstorage.blob.core.windows.net/edgewebdriver/LATEST_STABLE" --ssl-no-revoke
+    while [[ ! -f "${ls}" ]]; do sleep .2; done
+    v=$(iconv -f CP1252 -t UTF8 "$ls" | sed 's/[^0-9.]//g')
+    if [[ $OS == mac ]]; then OSD="${OS}64_m1"; else OSD=$OS; fi
+    URL="https://msedgedriver.azureedge.net/${v}/edgedriver_${OSD}.zip"
+    WEBDRIVER="msedgedriver"
+    ;;
+  chrome)
+    echo chrome
+    if [[ $OS == mac ]]; then OSD="${OS}-arm64"; else OSD=$OS; fi
+    URL="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/mac-arm64/chromedriver-${OSD}.zip"
+    WEBDRIVER="chromedriver"
+    ;;
+  firefox)
+    echo firefox
+    if [[ $OS == mac ]]; then
+      OSD="${OS}os-aarch64"
+      DOWNLOAD_EXTENSION="tar.gz"
+    elif [[ $OS == win64 ]]; then
+      OSD="win32"
+      DOWNLOAD_EXTENSION="tar.gz"
+    else OSD=$OS; fi
+    URL="https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-${OSD}.${DOWNLOAD_EXTENSION}"
+    WEBDRIVER="geckodriver"
+    ;;
+  *)
+    echo invalid
+    ;;
+  esac
+}
 
 # assign alias here
 function downloadWebDriver() {
 
-    DOWNLOAD_EXTENSION="zip"
-    PROJECT_DIR=${PWD}
-    TEST_RESOURCES="src/test/resources"
-    ABSPATH="${PROJECT_DIR}/${TEST_RESOURCES}/webDriver"
-    FILE_EXTENSION=""
+  DOWNLOAD_EXTENSION="zip"
+  PROJECT_DIR=${PWD}
+  TEST_RESOURCES="src/test/resources"
+  AP="${PROJECT_DIR}/${TEST_RESOURCES}/webDriver"
+  FILE_EXTENSION=""
+  BROWSER="$1"
 
-    case "$OSTYPE" in
-    solaris*) echo "$OSTYPE not supported" ;;
-    darwin*) OS='mac' ;;
-    linux*) OS='linux64' ;;
-    bsd*) echo "$OSTYPE not supported" ;;
-    msys*) OS='win64' FILE_EXTENSION=".exe" ;;
-    *) echo "unknown $OSTYPE" ;;
-    esac
+  case "$OSTYPE" in
+  solaris*) echo "$OSTYPE not supported" ;;
+  darwin*) OS='mac' ;;
+  linux*) OS='linux64' ;;
+  bsd*) echo "$OSTYPE not supported" ;;
+  msys*) OS='win64' FILE_EXTENSION=".exe" ;;
+  *) echo "unknown $OSTYPE" ;;
+  esac
 
-    case $1 in
-      edge)
-        echo edge
-        if [ $OS == 'mac' ]; then OSD="${OS}64_m1"; else OSD=$OS; fi
-        URL="https://msedgedriver.azureedge.net/120.0.2210.91/edgedriver_${OSD}.zip"
-        WEBDRIVER="msedgedriver" ;;
-      chrome)
-        echo chrome
-        if [ $OS == 'mac' ]; then OSD="${OS}-arm64"; else OSD=$OS; fi
-        URL="https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/mac-arm64/chromedriver-${OSD}.zip"
-        WEBDRIVER="chromedriver" ;;
-      firefox)
-        echo firefox
-        if [ $OS == 'mac' ]; then OSD="${OS}os-aarch64"; DOWNLOAD_EXTENSION="tar.gz";
-        elif [ $OS == 'win64' ]; then OSD="win32"; DOWNLOAD_EXTENSION="tar.gz";
-        else OSD=$OS; fi
-        URL="https://github.com/mozilla/geckodriver/releases/download/v0.34.0/geckodriver-v0.34.0-${OSD}.${DOWNLOAD_EXTENSION}"
-        WEBDRIVER="geckodriver" ;;
-      *)
-        echo invalid ;;
-    esac
+  download "$BROWSER"
 
-    if [ $WEBDRIVER == 'chromedriver' ];
-    then UNZPPDFILE="${WEBDRIVER}-${OSD}/${WEBDRIVER}${FILE_EXTENSION}";
-    else UNZPPDFILE=${WEBDRIVER}${FILE_EXTENSION}; fi
+  if [[ $WEBDRIVER == chromedriver ]]; then
+    UNZPPDFILE="${WEBDRIVER}-${OSD}/${WEBDRIVER}${FILE_EXTENSION}"
+  else UNZPPDFILE=${WEBDRIVER}${FILE_EXTENSION}; fi
 
-    FILE="driver.${DOWNLOAD_EXTENSION}"
+  FILE="driver.${DOWNLOAD_EXTENSION}"
 
-# checks to see if driver exists in project path or exists as defined in pom.xml property
-    if [[ (! -f ${ABSPATH}/${OS}/${WEBDRIVER}${FILE_EXTENSION}) && (! -f "${2}") ]]; then
-        cd /etc/.. && cd "${ABSPATH}" &&
-        mkdir "temp" &&
-        cd "temp" && echo $URL
-        curl -L -k --output $FILE $URL --ssl-no-revoke
-        if [[ $DOWNLOAD_EXTENSION == *zip ]]; then unzip $FILE; else tar xzf $FILE; fi;
-        cp "${UNZPPDFILE}" "${ABSPATH}/${OS}" &&
-        chmod "u=rwx,go=rx" "${ABSPATH}/${OS}/${WEBDRIVER}${FILE_EXTENSION}" &&
-        cd /etc/.. &&
-        cd "${ABSPATH}" &&
-        rm -rf temp &&
-        cd "$PROJECT_DIR" || exit
-    fi
+  # checks to see if driver exists in project path or exists as defined in pom.xml property
+  if [[ (! -f ${AP}/${OS}/${WEBDRIVER}${FILE_EXTENSION}) && (! -f "${2}") ]]; then
+    cd /etc/.. && cd "${AP}" &&
+      mkdir "temp" &&
+      cd "temp" && echo "$URL"
+    curl -L -k --output "$FILE" "$URL" --ssl-no-revoke
+    if [[ $DOWNLOAD_EXTENSION == *zip ]]; then unzip "$FILE"; else tar xzf "$FILE"; fi
+    cp "${UNZPPDFILE}" "${AP}/${OS}" &&
+      chmod "u=rwx,go=" "${AP}/${OS}/${WEBDRIVER}${FILE_EXTENSION}" &&
+      cd /etc/.. &&
+      cd "${AP}" &&
+      rm -rf "temp" &&
+      cd "$PROJECT_DIR" || exit
+  fi
 
-    echo $WEBDRIVER
-    echo "${ABSPATH}/${OS}/${WEBDRIVER}${FILE_EXTENSION}"
-    while [ ! -f "${ABSPATH}/${OS}/${WEBDRIVER}${FILE_EXTENSION}" ]; do sleep .2; done;
+  echo "$WEBDRIVER"
+  echo "${AP}/${OS}/${WEBDRIVER}${FILE_EXTENSION}"
+  while [ ! -f "${AP}/${OS}/${WEBDRIVER}${FILE_EXTENSION}" ]; do sleep .2; done
 }
 
 # downloadWebDriver edge
-
 
 #}
 #set -x
@@ -94,7 +108,7 @@ function downloadWebDriver() {
 ## shellcheck disable=SC2154
 #const SCRIPT_DIR="${realpath}"
 #const TEST_RESOURCES="/common/src/test/resources"
-#export ABSPATH="${SCRIPT_DIR}""${TEST_RESOURCES}"
+#export AP="${SCRIPT_DIR}""${TEST_RESOURCES}"
 #FILE_EXTENSION=""
 #
 #case "$OSTYPE" in
@@ -106,7 +120,7 @@ function downloadWebDriver() {
 #*) echo "unknown $OSTYPE" ;;
 #esac
 #
-#if [ ! -f "$ABSPATH/drivers/$OSTYPE/msedgedriver"$FILE_EXTENSION ]; then
+#if [ ! -f "$AP/drivers/$OSTYPE/msedgedriver"$FILE_EXTENSION ]; then
 #  cd "$TEST_RESOURCES/drivers" &&
 #    mkdir "temp" &&
 #    cd "temp" &&
@@ -115,7 +129,7 @@ function downloadWebDriver() {
 #    sleep 5
 #    cp "msedgedriver$FILE_EXTENSION" "$TEST_RESOURCES/drivers/$OSTYPE"
 #    chmod +700 "$TEST_RESOURCES/$OSTYPE/msedgedriver"$FILE_EXTENSION
-#    cd "$ABSPATH/drivers"
+#    cd "$AP/drivers"
 #    rm -rf temp &&
 #    cd "$ROOT_DIR" || exit
 #fi
